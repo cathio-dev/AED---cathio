46f6e0c7b6dab4d70aae02da04447369306d9d8f
cathio-dev <ramalho.marcos7@gmail.com>
2024-10-29 22:21:19 -0300

    29/10 - Anotações no código busca binária recursiva

--- AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/buscabinaria.c
+++ AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/buscabinaria.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int recbusca_binaria (int *V, int B, int ESQ, int DIR){
+int recbusca_binaria (int *V, int B, int ESQ, int DIR){ //algoritmo recursivo
     int meio;
-    if(ESQ>DIR){
+    if(ESQ>DIR){ //se esquerda for maior que direita (nao achou item)
         return -1;
     }
 
-    meio = (ESQ + DIR)/2;
+    meio = (ESQ + DIR)/2;  //meio é a média aritimetica entre esquerda e direita 0 + 6/2 = 3
 
-    if(V[meio]==B) return meio;
+    if(V[meio]==B) return meio; //se o conteudo em meio conteudo em 3 for o item procurado, retorna a posição no meio.
 
-    if(B<V[meio]){
+    if(B<V[meio]){ //se o item for menor que o valor do conteudo em meio, faz recursividade, eliminando toda a parte direita. ESQ = 0, DIR agora é = 2, temos 0, 1, 2 restantes.
         return recbusca_binaria(V, B, ESQ, meio - 1);
-    }else{
+    }else{              //se o item for maior, faremos o mesmo processo, só que ao contrário.
         return recbusca_binaria(V,B,meio+1,DIR);
     }
 
