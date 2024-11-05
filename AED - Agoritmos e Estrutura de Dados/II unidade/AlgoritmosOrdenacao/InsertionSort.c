e6ec450b3803acbcdaab37b90b4f52ef701f4d00
cathio-dev <ramalho.marcos7@gmail.com>
2024-11-04 23:03:58 -0300

    Insertion errado

--- AED - Agoritmos e Estrutura de Dados/II unidade/AlgoritmosOrdenacao/InsertionSort.c
+++ AED - Agoritmos e Estrutura de Dados/II unidade/AlgoritmosOrdenacao/InsertionSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-void insercao(int vet, int tam){
+void insercao(int *vet, int tam){
     int i, j, x;
 
     for (i=2; i<=tam; i++){
@@ -19,5 +19,17 @@ void insercao(int vet, int tam){
 
 
 int main(){
+    int vet[]={4,7,8,3,6,1,2,5};
+    int n=8;
+
+    insercao(vet, n); //vetor e numero de elementos.
+    
+    for(int i=0; i<n; i++ ){
+        if(i==7){
+            printf("%d.", vet[7]);
+            return 0;
+        }
+        printf("%d, ", vet[i]);
+    }
 
 }
