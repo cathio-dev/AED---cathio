4c42ec680a0f7b05b07c732c8c89c44a2ab8466c
cathio-dev <ramalho.marcos7@gmail.com>
2024-11-19 09:41:40 -0300

    ALT + SHIFT + F

--- AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/1191BB.c
+++ AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/1191BB.c
@@ -4,11 +4,12 @@
 
 #define MAX 27
 
-int indprefixa;
+int indprefixa; // indice da string prefixa. caso prefixo: DBACEGF // caso infixo: ABCDEFG
 
 typedef char tp_item;
 
-typedef struct tp_no{
+typedef struct tp_no
+{
     struct tp_no *esq;
     tp_item info;
     struct tp_no *dir;
@@ -21,63 +22,66 @@ tp_arvore inicializa_arvore()
     return NULL;
 }
 
-tp_no *aloca_no() //aloca e retorna o endereço
+tp_no *aloca_no() // aloca e retorna o endereço
 {
-	tp_no *no;
-	no=(tp_no * )malloc(sizeof(tp_no));
-	return no;
+    tp_no *no;
+    no = (tp_no *)malloc(sizeof(tp_no));
+    return no;
 }
 
-void pos_ordem (tp_no *p) { //imprime os elementos em pós-ordem
-if (p != NULL) {
-pos_ordem (p->esq);
-pos_ordem (p->dir);
-printf("%c",p->info);
-}
+void pos_ordem(tp_no *p)
+{ // imprime os elementos em pós-ordem
+    if (p != NULL)
+    {
+        pos_ordem(p->esq);
+        pos_ordem(p->dir);
+        printf("%c", p->info);
+    }
 }
 
 int busca_infixa(char *infixa, int esq, int dir, tp_item info)
 {
-int i;
-for (i = esq; i <= dir; i++)
-if (infixa[i] == info)
-return i;
-return -1;
+    int i;
+    for (i = esq; i <= dir; i++)
+        if (infixa[i] == info)
+            return i;
+    return -1;
 }
 
 tp_arvore monta_arvore(char prefixa[], char infixa[], int esq, int dir)
 {
-if (esq > dir)
-return NULL;
+    if (esq > dir)
+        return NULL;
 
-//Aloca memória para um nó, preenche com o nó raiz
-tp_no *no = aloca_no();
-no->info = prefixa[indprefixa];
-no->dir = no->esq = NULL;
-indprefixa++;
+    // Aloca memória para um nó, preenche com o nó raiz
+    tp_no *no = aloca_no();
+    no->info = prefixa[indprefixa];
+    no->dir = no->esq = NULL;
+    indprefixa++;
 
-if (esq == dir) // Se nó folha, retorna o end. do nó para a chamada anterior
-	return no;
+    if (esq == dir) // Se nó folha, retorna o end. do nó para a chamada anterior
+        return no;
 
-int indinfixa = busca_infixa(infixa, esq, dir, no->info);
-no->esq = monta_arvore(prefixa, infixa, esq, indinfixa-1);
-no->dir = monta_arvore(prefixa, infixa, indinfixa+1, dir);
+    int indinfixa = busca_infixa(infixa, esq, dir, no->info);
+    no->esq = monta_arvore(prefixa, infixa, esq, indinfixa - 1);
+    no->dir = monta_arvore(prefixa, infixa, indinfixa + 1, dir);
 
-return no;
+    return no;
 }
 
-int main ()
+int main()
 {
-tp_arvore raiz;
-raiz=inicializa_arvore();
-char prefixa[MAX];
-char infixa[MAX];
+    tp_arvore raiz;
+    raiz = inicializa_arvore();
+    char prefixa[MAX];
+    char infixa[MAX];
 
-while (scanf("%s %s", prefixa, infixa) != EOF) {
-	indprefixa = 0;
-	raiz = monta_arvore(prefixa, infixa, 0, strlen(prefixa)-1);
-	pos_ordem(raiz);
-	printf("\n");
-}
-return 0;
+    while (scanf("%s %s", prefixa, infixa) != EOF)
+    {
+        indprefixa = 0;
+        raiz = monta_arvore(prefixa, infixa, 0, strlen(prefixa) - 1);
+        pos_ordem(raiz);
+        printf("\n");
+    }
+    return 0;
 }
