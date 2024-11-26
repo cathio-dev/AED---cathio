4c0510181398c29da6478ee8a19c4bb61575f3de
cathio-dev <ramalho.marcos7@gmail.com>
2024-11-26 09:35:24 -0300

    Arvore BB funcionando

--- AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/ArvoreBinaria.h
+++ AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/ArvoreBinaria.h
@@ -1,8 +1,7 @@
-#ifndef ARVOREBINARIA_H
-#define ARVOREBINARIA_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#ifndef ArvoreBinaria.h
+#define ArvoreBinaria.h
 
 typedef int tp_item;
 
@@ -12,185 +11,183 @@ typedef struct tp_no{
     struct tp_no *dir;
 }tp_no;
 
-typedef tp_no *tp_arvore;
+typedef struct tp_arvore {
+tp_no *raiz;
+int quantidade;
+} tp_arvore;
 
-tp_arvore inicializa_arvore(){
-    return NULL;
+tp_arvore* inicializa_arvore() {
+tp_arvore* arvore = (tp_arvore*) malloc(sizeof(tp_arvore));
+arvore->raiz = NULL;
+arvore->quantidade = 0;
+return arvore;
 }
 
-int arvore_vazia(tp_arvore raiz){
-    if(raiz==NULL) return 1;
-    else return 0;
+int arvore_vazia(tp_arvore *arvore) {
+return (arvore->raiz == NULL);
 }
 
-tp_no *aloca_no(){
-    tp_no *no;
-    no=(tp_no*)malloc(sizeof(tp_no));
-    return no;
+tp_no *aloca_no() {
+tp_no *no = (tp_no *)malloc(sizeof(tp_no));
+return no;
 }
 
-int insere_no(tp_arvore *raiz, tp_item e){
-    tp_no *pai = NULL, *novo, *p=*raiz;
-    novo=aloca_no();
-    if(!novo) return 0;
+int insere_no(tp_arvore *arvore, tp_item e) {
+tp_no *pai = NULL, *novo, *p = arvore->raiz;
+novo = aloca_no();
+if (!novo) return 0;
 
-    novo->info=e;
-    novo->esq=NULL;
-    novo->dir=NULL;
-    while(p!=NULL){
-        pai=p;
-        if( e < p-> info)
-            p=p->esq;
-        else
-            p=p->dir;
-    }
+novo->info = e;
+novo->esq = NULL;
+novo->dir = NULL;
 
-    if(pai != NULL){
-        if(e < pai->info)
-            pai->esq=novo;
-        else
-            pai -> dir = novo;
-    }else{
-        *raiz = novo;
-        return 1;
-    }
+while (p != NULL) {
+    pai = p;
+    if (e < p->info)
+        p = p->esq;
+    else
+        p = p->dir;
+}
+
+if (pai != NULL) {
+    if (e < pai->info)
+        pai->esq = novo;
+    else
+        pai->dir = novo;
+} else {
+    arvore->raiz = novo;
+}
+
+arvore->quantidade++;
+return 1;
 }
 
 void pre_ordem(tp_no *p){
     if(p!=NULL){
-        printf("\n%d\n", p->info); //visita antes o no, depois esq, dps direita. começa imprimindo o pai.
-        pre_ordem(p->esq);
-        pre_ordem(p->dir);
-
+    printf("\n%d\n",p->info);
+    pre_ordem(p->esq);                                                                  
+    pre_ordem(p->dir);
     }
+    
 }
-
 void em_ordem(tp_no *p){
-    if(p!=NULL){
-        em_ordem(p->esq);
-        printf("\n%d\n", p->info); //visita antes o no, depois esq, dps direita. começa imprimindo o pai.
-        em_ordem(p->dir);
-
+     if(p!=NULL){
+    em_ordem(p->esq); 
+    printf("\n%d\n",p->info);
+    em_ordem(p->dir);
     }
 }
-
-void pos_ordem(tp_no *p){
+    void pos_ordem(tp_no *p){
     if(p!=NULL){
-        pos_ordem(p->esq);
-        pos_ordem(p->dir);
-        printf("\n%d\n", p->info); //visita antes o no, depois esq, dps direita. começa imprimindo o pai.
+    pos_ordem(p->esq);                                                                  
+    pos_ordem(p->dir);
+     printf("\n%d\n",p->info);
     }
+    
+}
+tp_no *busca_no(tp_no *p, tp_item e) {
+while (p != NULL) {
+if (e < p->info)
+p = p->esq;
+else if (e > p->info)
+p = p->dir;
+else
+return p;
+}
+return NULL;
 }
 
-tp_no* busca_no (tp_no *p, tp_item e){
-    while (p != NULL){ //enquanto no pai for dferente que null
-        if(e<p->info) //se o elemento for menor que o no, o iremos para o no da esquerda. repetimos. Entramos no nó da esquerda;
-            p=p->esq;
-        else            //se for maior, vamos para o filho da direita.
-            if(e>p->info)       
-                p = p->dir;
-            else                //se for igual, retornamos o próprio nó. que será o nó do topo.
-                return p;      //retornamos o p.
+tp_no*  busca_noRec(tp_no *p, tp_item e){
+    if(p!=NULL){
+        if(e<p->info){
+        busca_noRec(p->esq,e);
+        }else if(e<p->info){
+        busca_noRec(p->dir,e);
     }
-    return (NULL);
+    else return p;
+        }
+        else return NULL;
+}
+
+int altura_arvore(tp_no *raiz) {
+    return (qntd_niveis(raiz) - 1);
+}
+    
+
+int qntd_niveis(tp_no *raiz){
+    if(raiz==NULL)return 0;
+    int alt_esq= altura_arvore(raiz->esq)+1;
+    int alt_dir= altura_arvore(raiz->dir)+1;
+    if(alt_esq> alt_dir){
+        return alt_esq;
+    }else{
+        return alt_dir;
+    }
     
 }
 
-int qnt_niveis(tp_arvore raiz){
-    if(raiz == NULL) return 0;
-    int alt_esq = qnt_niveis(raiz -> esq);
-    int alt_dir = qnt_niveis(raiz -> dir);
-    if(alt_esq > alt_dir){
-        return alt_esq + 1;
-    }else{
-        return alt_dir + 1;
+
+int qntd_nos(tp_no *p){
+  int i=0;
+  if(p!=NULL){
+    i+=qntd_nos(p->esq);
+    i++;
+    i+=qntd_nos(p->dir);
+  }
+  return i;
+}
+int remove_no(tp_arvore *arvore, tp_item e) {
+tp_no *p = arvore->raiz, *ant = NULL, *sub, *pai, *filho;
+while (p != NULL && p->info != e) {
+ant = p;
+if (e < p->info)
+p = p->esq;
+else
+p = p->dir;
+}
+if (p == NULL) return 0;
+
+if (p->esq == NULL)
+    sub = p->dir;
+else if (p->dir == NULL)
+    sub = p->esq;
+else {
+    pai = p; sub = p->dir; filho = sub->esq;
+    while (filho != NULL) {
+        pai = sub;
+        sub = filho;
+        filho = sub->esq;
     }
-}
-
-int altura_arvore(tp_arvore raiz){
-    return (qnt_niveis(raiz) - 1);
-}
-
-/***** Fila *****/
-
-typedef struct tp_fila {
-	tp_no *no;
-	struct tp_fila *prox;	
-} tp_fila;
-
-tp_fila * inicia_fila() {
-	return NULL;
-}
-
-int fila_vazia(tp_fila **fila) {
-	if(*fila == NULL) return 1;
-	return 0;
-}
-
-tp_fila * aloca_fila(tp_no *no) {
-	tp_fila *no_fila = (tp_fila*) malloc(sizeof(tp_fila));
-	no_fila->no = no;
-	no_fila->prox = NULL;
-	return no_fila;
-}
-
-int insere_fila(tp_fila **fila, tp_no *no) {
-	tp_fila *no_fila = aloca_fila(no), *atu = *fila;
-	if(no_fila == NULL) return 0;
-	
-	if(fila_vazia(fila)) *fila = no_fila;
-	else {
-		while(atu->prox != NULL) {
-			atu = atu->prox;
-		}
-		atu->prox = no_fila;
-	}
-	return 1;
-}
-
-tp_no * remove_fila(tp_fila **fila) {
-	tp_fila *atu = *fila;
-	*fila = atu->prox;
-	tp_no *no = atu->no;
-	free(atu);
-	atu = NULL;
-	return no;
-}
-
-void imprime_fila(tp_fila **fila) {
-	tp_fila *atu = *fila;
-	while(atu != NULL) {
-		printf("%d ", atu->no->info);
-		atu = atu->prox;
-	}
-	printf("\n");
-}
-
-void destroi_fila(tp_fila **fila) {
-	tp_fila *atu = *fila;
-	while(atu != NULL) {
-		*fila = atu->prox;
-		free(atu);
-		atu = *fila;
-	}
-	*fila = NULL;
-}
-
-
-/*void BFS(tp_arvore *raiz) {
-    if(!arvore_vazia(*raiz)) {
-        tp_fila *fila = inicia_fila();
-        insereFila(&fila, *raiz);
-
-	    while(!fila_vazia(&fila)) {
-		    tp_no *no = remove_fila(&fila);
-		    printf("%d ", no->info);
-		
-		    if(no->esq != NULL) insere_fila(&fila, no->esq);
-		    if(no->dir != NULL) insere_fila(&fila, no->dir);
-	    }
-	destroi_fila(&fila);
+    if (pai != p) {
+        pai->esq = sub->dir;
+        sub->dir = p->dir;
     }
-}*/
+    sub->esq = p->esq;
+}
+if (ant == NULL)
+    arvore->raiz = sub;
+else if (p == ant->esq)
+    ant->esq = sub;
+else
+    ant->dir = sub;
+
+free(p);
+arvore->quantidade--;
+return 1;
+}
+
+void destroi_no(tp_no *no) {
+if (no == NULL) return;
+destroi_no(no->esq);
+destroi_no(no->dir);
+free(no);
+}
+
+void destroi_arvore(tp_arvore *arvore) {
+destroi_no(arvore->raiz);
+free(arvore);
+}
+
+
 
 #endif
--- AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/testeArvoreBB.c
+++ AED - Agoritmos e Estrutura de Dados/II unidade/PesquisaEmMemoria/testeArvoreBB.c
@@ -1,34 +1,78 @@
 #include <stdio.h>
 #include "ArvoreBinaria.h"
+int main(){
+    
+tp_arvore *arv;
+arv=inicializa_arvore();
+    int esc=0,e,elem;
 
-int main()
-{
-    tp_arvore *arvore = inicializa_arvore;
-
-    // Inserir elementos na árvore
-    insere_no(arvore, 10);
-    insere_no(arvore, 5);
-    insere_no(arvore, 15);
-    insere_no(arvore, 3);
-    insere_no(arvore, 7);
-
-    if (arvore_vazia(arvore) == 1)
-    {
-        printf("Arvore vazia");
+do{
+    printf("escolha uma opção:\n1- inserir\n2-printar\n3-buscar\n4-buscarec\n5-quant nos\n6-altura\n7-quant niveis\n8-remove no\n9-destroi arvore\n10-sair\n");
+    scanf("%d",&esc);
+    
+    switch (esc) {
+        case 1:
+        printf("digite o valor a ser inserido:");
+        scanf("%d",&e);
+        insere_no(arv,e);
+        
+        break;
+         case 2:
+         
+         printf("pre ordem: ");
+         pre_ordem(arv->raiz);
+         printf("\n");
+         printf("em ordem: ");
+         em_ordem(arv->raiz);
+         printf("\n");
+         printf("pos ordem: ");
+         pos_ordem(arv->raiz);
+         
+         break;
+         
+        case 3:
+        scanf("%d",&elem);
+       if (busca_no(arv->raiz,elem))
+       printf("achei\n");
+       else printf("nada\n");
+        break;
+        
+        case 4:
+        scanf("%d",&elem);
+       if (busca_noRec(arv->raiz,elem))
+       printf("achei\n");
+       else printf("nada\n");
+        break;
+        
+        case 5:
+        printf("%d\n",qntd_nos(arv->raiz));
+        break;
+        
+        case 6:
+        printf("%d\n",altura_arvore(arv->raiz));
+        break;
+        
+        case 7:
+        printf("%d\n",qntd_niveis(arv->raiz));
+        break;
+        
+         case 8:
+       scanf("%d",&elem);
+        remove_no(arv,elem);
+        break;
+        
+        case 9:
+        destroi_arvore(arv);
+        break;
+        
+        default:
+        scanf("%d",&esc);
+        
     }
-    else
-    {
-        printf("Arvore conteudista");
-    }
+    
+}while(esc != 10);
 
-    em_ordem(arvore);
-    /*// Exibir a árvore em ordem e a quantidade de nós
-    printf("Árvore em ordem: ");
-    em_ordem(arvore);
-    //printf("\nQuantidade de nós: %d\n", arvore->quantidade);
+return 0;
 
-    // Liberar memória
-    destroi_arvore(arvore);*/
-
-    return 0;
 }
+
